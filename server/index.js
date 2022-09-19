086751069a35f45e4778ce2cbe20e8ad74139cd1
dropiyo <lomakinia@studklg.ru>
2022-09-19 19:04:24 +0300

    lab2_done

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const app = express();
+const port = 3000; 
+app.listen(port, () => {
+    console.log('Сервер запущен. Порт сервера: ${port}');
+});
+app.get("/", (req, res) => {
+    res.send("HELLO MAN!");
+});
